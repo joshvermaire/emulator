2ada0a1c607a79fc474f428733dd6473bcd9691a
Josh Vermaire <joshvermaire@gmail.com>
2014-02-18 10:42:39 -0800

    add a few tests

--- test/index.js
+++ test/index.js
@@ -27,12 +27,33 @@ describe('index', function () {
     });
 
 
-    it('should say "hello"', function (done) {
+    it('GET /profile?user=me should give {user:"me"}', function (done) {
         request(mock)
-            .get('/')
+            .get('/profile?user=me')
             .expect(200)
-            .expect('Content-Type', /html/)
-            .expect(/Hello, /)
+            .expect('Content-Type', /json/)
+            .expect('{"user":"me"}')
+            .end(function(err, res){
+                done(err);
+            });
+    });
+    it('GET /profile?user=me&response[user]=you should give {user:"you"}', function (done) {
+        request(mock)
+            .get('/profile\?user\=me\&response\[user\]\=you')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('{"user":"you"}')
+            .end(function(err, res){
+                done(err);
+            });
+    });
+    it('POST {user:"me"} to /profile  should give {user:"me"}', function (done) {
+        request(mock)
+            .post('/profile')
+            .send({ user: 'me' })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('{"user":"me"}')
             .end(function(err, res){
                 done(err);
             });
